a1fab125bdad12881d76de019d3243873c6e3c9f
Evan Smith <esincork@gmail.com>
2015-06-25 17:00:53 +0100

    Finished Go implementation

--- Go/main.go
+++ Go/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
-	// "io/ioutil"
 	"bytes"
 	"strings"
+	"math/rand"
+	"time"
 )
 
 // Performs the main test execution of the program
@@ -16,6 +17,9 @@ func main() {
 	markov := Markov{ }
     markov.openFile( )
     markov.splitWords( )
+    markov.storeProbability( )
+    text := markov.generateMarkovText( 250 )
+    fmt.Println( text )
 }
 
 // A 2nd Order Markov Chain Text Generator
@@ -23,6 +27,7 @@ type Markov struct{
 	store map[string][]string
 	words []string
 	fileContents string
+	keys []string
 }
 
 // Opens the file for reading and converts it from bytes
@@ -48,4 +53,58 @@ func (markov *Markov) openFile() {
 // Splits all of the words up into a string slice
 func (markov *Markov) splitWords( ){
 	markov.words = strings.Split(markov.fileContents, " ")
+}
+
+// Creates a simple 2nd Order probability table for the words
+func (markov *Markov) storeProbability( ){
+	markov.store = make( map[string][]string )
+
+	wordSize := len(markov.words)
+
+	if wordSize < 3{
+		// If there's less than three words, don't bother
+		return
+	} else {
+		for i := 0; i < wordSize - 2; i++ {
+			// Uses first and second word as a key, simulating probablity
+			// by appending all succeeding words to an array
+			key := markov.words[ i ] + "," + markov.words[ i + 1 ]
+			markov.keys = append( markov.keys, key )
+			markov.store[ key ] = append( markov.store[ key ], markov.words[ i + 2 ] )
+		}
+	}
+}
+
+// Generate the markov chain text based on the calculated store
+func (markov Markov) generateMarkovText( wordLimit int ) string{
+	output := ""
+	keysLength := len( markov.keys )
+
+	if !( len(markov.store) == 0 ){
+		// Make sure the store isn't empty before beginning
+
+		rand.Seed( time.Now().UnixNano() )
+		randomInt := rand.Intn( keysLength )
+		key := markov.keys[ randomInt ]
+
+		for i := 0; i < wordLimit; i++ {
+			words := strings.Split( key, "," )
+			output += words[ 0 ] + " "
+
+			keyPossibilities := len( markov.store[key] )
+			if keyPossibilities < 1{
+				// Randomly choose a possibility from the key's position in the store
+				rand.Seed( time.Now().UnixNano() )
+				randomInt = rand.Intn( len( markov.store[key] ) )
+				key = words[1] + "," + markov.store[key][randomInt]
+			} else{
+				// If the key has 0 possibilities, randomly choose a new key to work with
+				rand.Seed( time.Now().UnixNano() )
+				randomInt := rand.Intn( keysLength )
+				key = markov.keys[ randomInt ]
+			}
+		}
+	}
+
+	return strings.Replace( output, "\n", " ", -1)
 }
